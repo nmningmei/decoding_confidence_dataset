89fb112422a6523607de2fda307901e9049b2ee0
nmningmei <semsociaty@gmail.com>
2021-03-10 12:46:21 +0800

    bash jobs

--- scripts/3.2.2.simple_RNN_LOSO_to_bash.py
+++ scripts/3.2.2.simple_RNN_LOSO_to_bash.py
@@ -10,7 +10,7 @@ import pandas as pd
 from shutil import copyfile
 
 template            = '3.2.1.simple_RNN_LOSO.py'
-experiment          = ['confidence','LOO','RNN']
+experiment          = ['adequacy','LOO','RNN']
 data_dir            = '../data'
 working_df_name     = os.path.join(data_dir,experiment[0],experiment[1],'all_data.csv')
 df_def              = pd.read_csv(working_df_name,)
