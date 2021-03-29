995bafdfe9657c844c6761d461240525a1ee59ac
nmningmei <semsociaty@gmail.com>
2021-03-29 12:52:03 +0800

    update

--- scripts/5.1.1.stats_LOO.py
+++ scripts/5.1.1.stats_LOO.py
@@ -22,15 +22,20 @@ import seaborn as sns
 
 from matplotlib import pyplot as plt
 
+from sklearn import linear_model
+from sklearn.model_selection import (LeaveOneGroupOut,
+                                     permutation_test_score,
+                                     cross_validate)
+
 sns.set_style('whitegrid')
 sns.set_context('poster')
 
-experiment  = 'confidence' # confidence or adequacy
+experiment  = 'adequacy' # confidence or adequacy
 cv_type     = 'LOO' # LOO or cross_domain
 decoder     = 'regression' #
 working_dir = f'../results/{experiment}/{cv_type}/'
 stats_dir   = f'../stats/{experiment}/{cv_type}'
-figures_dir = f'../stats/{experiment}/{cv_type}'
+figures_dir = f'../figures/{experiment}/{cv_type}'
 if not os.path.exists(stats_dir):
     os.makedirs(stats_dir)
 if not os.path.exists(figures_dir):
@@ -50,9 +55,9 @@ ylim    = {'confidence':(0.45,0.9),
            'adequacy':(0.3,0.9)}[experiment]
 confidence_range= 4
 time_steps      = np.array([f'T-{7-ii}' for ii in range(7)])
-dict_rename     = {0:'Incorrect trials',1:'Correct trials'}
+dict_rename     = {0:'incorrect trials',1:'correct trials'}
 xargs           = dict(hue          = 'accuracy_test',
-                       hue_order    = ['Correct trials','Incorrect trials',],
+                       hue_order    = ['correct trials','incorrect trials',],
                        split        = True,
                        inner        = 'quartile',
                        cut          = 0,
@@ -146,47 +151,113 @@ g.savefig(os.path.join(figures_dir,'scores.jpg'),
           dpi = 300,
           bbox_inches = 'tight')
 
-fig,axes = plt.subplots(figsize = (10*2,6),
-                        ncols = 2,
-                        )
+
 # get the weights of the regression model
 df_reg = df_ave[df_ave['decoder'] == decoder]
-for idx_ax,((acc_train),df_sub) in enumerate(df_reg.groupby(['accuracy_train'])):
-    ax = axes.flatten()[idx_ax]
+
+results = dict(accuracy_train = [],
+               coefficient = [],
+               ps = [],
+               )
+results[experiment] = []
+slopes = dict(accuracy_train = [],
+              coefficient = [],
+              intercept = [],
+              )
+slopes[experiment] = []
+# fit a regression to show the linear trend of the weights
+for (acc_train),df_sub in df_reg.groupby(['accuracy_train']):
+    for idx_confidence in range(confidence_range):
+        weight_for_fit = df_sub[[f'weight T-{7-ii} C-{idx_confidence}' for ii in range(len(time_steps))]].values
+        xx = np.vstack([np.arange(7) for _ in range(weight_for_fit.shape[0])])
+        groups = df_sub['fold'].values
+        groups = np.vstack([groups for _ in range(len(time_steps))]).T
+        cv = LeaveOneGroupOut()
+        pipeline = linear_model.BayesianRidge(fit_intercept = True)
+        # permutation test to get p values
+        _score,_,pval_slope = permutation_test_score(pipeline,
+                                                     xx.reshape(-1,1),
+                                                     weight_for_fit.reshape(-1,1).ravel(),
+                                                     cv = cv,
+                                                     groups = groups.reshape(-1,1).ravel(),
+                                                     n_jobs = -1,
+                                                     random_state = 12345,
+                                                     n_permutations = int(1e4),
+                                                     scoring = 'neg_mean_squared_error',
+                                                     verbose = 1,
+                                                     )
+        # cross validation to get the slopes and intercepts
+        gc.collect()
+        _res = cross_validate(pipeline,
+                              xx.reshape(-1,1),
+                              weight_for_fit.reshape(-1,1).ravel(),
+                              groups = groups.reshape(-1,1).ravel(),
+                              cv = cv,
+                              n_jobs = -1,
+                              verbose = 1,
+                              scoring = 'neg_mean_squared_error',
+                              return_estimator = True,
+                              )
+        gc.collect()
+        coefficients = np.array([est.coef_[0] for est in _res['estimator']])
+        intercepts = np.array([est.intercept_ for est in _res['estimator']])
+        results['accuracy_train'].append(acc_train)
+        results[experiment].append(idx_confidence)
+        results['ps'].append(pval_slope)
+        results['coefficient'].append(np.mean(coefficients))
+        for coef_,inte_ in zip(coefficients,intercepts):
+            slopes['accuracy_train'].append(acc_train)
+            slopes[experiment].append(idx_confidence)
+            slopes['coefficient'].append(coef_)
+            slopes['intercept'].append(inte_)
+results = pd.DataFrame(results)
+slopes = pd.DataFrame(slopes)
+
+results.to_csv(os.path.join(stats_dir,'features.csv'),index = False)
+slopes.to_csv(os.path.join(stats_dir,'slopes.csv'),index = False)
+
+df_weights = []
+for acc_train in ['correct trials','incorrect trials']:
+    df_sub = df_reg[df_reg['accuracy_train'] == acc_train]
     weights = df_sub[[col for col in df_sub.columns if ('weight' in col)]].values
     w = np.concatenate([[w.reshape(7,-1).T] for w in weights])
-
-    # plot the weights
-
-    colors = ['blue','orange','green','red']
     for ii in range(confidence_range):
         weight_for_plot = w[:,ii,:]
-        w_mean = weight_for_plot.mean(0)
-        w_std = weight_for_plot.std(0)
-        ax.plot(time_steps,
-                w_mean,
-                color = colors[ii],
-                alpha = 1.,
-                )
-        ax.fill_between(time_steps,
-                        w_mean + w_std,
-                        w_mean - w_std,
-                        color = colors[ii],
-                        alpha = .5,
-                        label = f'confidence {ii+1}')
-    ax.legend(loc = 'upper left')
-    ax.set(title = f'train on {acc_train}',
-           ylabel = 'Weight (A.U)',
-           xlabel = 'Trial')
-fig.savefig(os.path.join(figures_dir,'features.jpg'),
-            dpi = 300,
-            bbox_inches = 'tight')
+        temp = pd.DataFrame(weight_for_plot,columns = [f'T-{len(time_steps)-ii}' for ii in range(len(time_steps))])
+        temp['accuracy_train'] = acc_train
+        temp[experiment] = ii
+        df_weights.append(temp)
+df_weights = pd.concat(df_weights)
+df_weights_plot = pd.melt(df_weights,
+                          id_vars = ['accuracy_train',experiment],
+                          value_vars = [f'T-{len(time_steps)-ii}' for ii in range(len(time_steps))],
+                          var_name = ['Time'],
+                          value_name = 'Weights')
 
-x = np.vstack([np.arange(7) for _ in range(features.shape[0])])
-# fit a regression to show the linear trend of the weights
-for ii in range(confidence_range):
-    weight_for_fit = w[:,ii,:]
+colors = ['blue','orange','green','red']
+df_weights_plot['x'] = df_weights_plot['Time'].apply(lambda x:-int(x[-1]))
 
+g = sns.lmplot(x = 'x',
+               y = 'Weights',
+               hue = experiment,
+               col = 'accuracy_train',
+               col_order = xargs['hue_order'],
+               data = df_weights_plot,
+               palette = colors,
+               markers = '.',
+               robust = True,
+               seed = 12345,
+               aspect = 1.5,
+               )
+(g.set_titles("train on {col_name}")
+  .set_axis_labels('Trial','Weights (A.U.)')
+  .set(xlim = (-7.5,-0.5),
+       xticks = np.arange(-7,0),
+       xticklabels = [f'T-{len(time_steps)-ii}' for ii in range(len(time_steps))])
+  )
+g.savefig(os.path.join(figures_dir,'features.jpg'),
+          dpi = 300,
+          bbox_inches = 'tight')
 
 
 
