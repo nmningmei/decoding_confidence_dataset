7974c5277be285a582adec26dd4af2dfd142d35e
nmningmei <semsociaty@gmail.com>
2021-03-30 03:50:20 +0200

    update

--- scripts/4.2.1.simple_RNN_cross_domain_confidence.py
+++ scripts/4.2.1.simple_RNN_cross_domain_confidence.py
@@ -11,22 +11,19 @@ import gc
 gc.collect() # clean garbage memory
 from glob import glob
 
+import tensorflow as tf
+from tensorflow.keras import Model
 from tensorflow.keras.utils import to_categorical
-from sklearn.svm import LinearSVC
-from sklearn.calibration import CalibratedClassifierCV
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import LeaveOneGroupOut
 
 import numpy  as np
 import pandas as pd
 
-from utils import scoring_func
-from functools import partial
-from scipy.special import softmax
+from utils import build_RNN,scoring_func
 
-experiment          = ['confidence','cross_domain','regression']
-property_name       = 'weight' # or hidden states or weight
+from sklearn.model_selection import StratifiedShuffleSplit,LeaveOneGroupOut
+
+experiment          = ['confidence','cross_domain','RNN']
+property_name       = 'hidden states' # or hidden states or weight
 data_dir            = '../data/'
 model_dir           = os.path.join('../models',experiment[0],experiment[1],)
 source_dir          = '../data/4-point'
@@ -51,7 +48,7 @@ df_target           = pd.read_csv(target_df_name)
 
 df_target['domain'] = df_target['filename'].apply(lambda x:x.split('/')[3].split('-')[0])
 
-results             = dict(fold             = [],
+results             = dict(
                            score            = [],
                            n_sample         = [],
                            source           = [],
@@ -61,50 +58,73 @@ results             = dict(fold             = [],
                            accuracy_test    = [],
                            )
 for ii in range(time_steps):
-    for jj in range(confidence_range):
-        results[f'{property_name} T-{time_steps - ii} C-{jj}'] = []
+    results[f'{property_name} T-{time_steps - ii}'] = []
 
 features    = df_source[[f"feature{ii + 1}" for ii in range(time_steps)]].values
 targets     = df_source["targets"].values.astype(int)
-groups      = df_source["filename"].values#.apply(lambda x:x.split('/')[-1].split('.')[0]) + "_" + df_source['sub'].astype(str)
+groups      = df_source["sub"].values
 accuracies  = df_source['accuracy'].values
 
 csv_saving_name     = os.path.join(result_dir,f'{experiment[-1]}_{experiment[0]} results.csv')
+
+csv_saving_name     = os.path.join(result_dir,f'{experiment[-1]}_{experiment[0]} results.csv')
 cv = LeaveOneGroupOut()
-for fold,(_,train) in enumerate(cv.split(features,targets,groups = groups)):
+for fold,(_,_train) in enumerate(cv.split(features,targets,groups = groups)):
     for acc_trial_train in [0,1]:
-        _idx_train, = np.where(accuracies[train] == acc_trial_train)
-        X_,Y_,Z_ = features[train][_idx_train],targets[train][_idx_train],groups[train][_idx_train]
+        _idx_train, = np.where(accuracies[_train] == acc_trial_train)
+        X_,Y_,Z_ = features[_train][_idx_train],targets[_train][_idx_train],groups[_train][_idx_train]
+        # the for-loop does not mean any thing, we only take the last step/output of the for-loop
+        for train,valid in StratifiedShuffleSplit(test_size = 0.2,random_state = 12345).split(X_,Y_,Z_):
+            X_train,y_train,group_train,acc_train = X_[train],Y_[train],Z_[train],accuracies[train]
+            X_valid,y_valid,gruop_valid,acc_valid = X_[valid],Y_[valid],Z_[valid],accuracies[valid]
         
-        print('fitting ...')
-        svc = LinearSVC(random_state = 12345)
-        model = CalibratedClassifierCV(svc,cv = 5)
-        model = make_pipeline(StandardScaler(),model)
-        model.fit(X_,Y_)
+        X_train = to_categorical(X_train - 1, num_classes = confidence_range)
+        X_valid = to_categorical(X_valid - 1, num_classes = confidence_range)
         
-        print(f'get {property_name}')
-        properties = np.concatenate([est.base_estimator.coef_[np.newaxis] for est in model.steps[-1][-1].calibrated_classifiers_]).mean(0)
+        y_train = to_categorical(y_train - 1, num_classes = confidence_range)
+        y_valid = to_categorical(y_valid - 1, num_classes = confidence_range)
+        
+        model_name     = os.path.join(model_dir,f'{"_".join(experiment)}.h5')
+        model,callbacks = build_RNN(time_steps,confidence_range,model_name)
+        
+        model.fit(X_train,
+                  y_train,
+                  batch_size        = batch_size,
+                  epochs            = 1000,
+                  validation_data   = (X_valid,y_valid),
+                  shuffle           = True,
+                  callbacks         = callbacks,
+                  verbose           = 1,
+                  )
+        del model
+        model = tf.keras.models.load_model(model_name)
+        # freeze the model
+        for layer in model.layers:
+            layer.trainable = False
+        hidden_model = Model(model.input,model.layers[1].output)
         
         # test phase
         for (filename,sub_name,target_domain),df_sub in df_target.groupby(['filename','sub','domain']):
             df_sub
             features_        = df_sub[[f"feature{ii + 1}" for ii in range(7)]].values
             targets_         = df_sub["targets"].values.astype(int)
+            groups_          = df_sub["sub"].values
             X_test,y_test    = features_.copy(),targets_.copy()
             acc_test         = df_sub['accuracy'].values
-            # X_test           = to_categorical(X_test - 1, num_classes = confidence_range).reshape(-1,time_steps*confidence_range)
+            X_test           = to_categorical(X_test - 1, num_classes = confidence_range)
             y_test           = to_categorical(y_test - 1, num_classes = confidence_range)
-            
-            preds_test  = model.predict_proba(X_test)
             
             for acc_trial_test in [0,1]:
                 _idx_test, = np.where(acc_test == acc_trial_test)
                 if len(_idx_test) > 1:
-                    score_test = scoring_func(y_test[_idx_test],preds_test[_idx_test],
-                                              confidence_range = confidence_range,
-                                              need_normalize = True,)
+                    preds_test  = model.predict(X_test.astype('float32')[_idx_test], batch_size=batch_size)
+                    print(f'get {property_name}')
+                    hidden_state_test,h_state_test,c_state_test = hidden_model.predict(X_test[_idx_test],
+                                                                                        batch_size = batch_size,
+                                                                                        verbose = 1)
+                    score_test = scoring_func(y_test[_idx_test],preds_test,
+                                              confidence_range = confidence_range)
                     print(score_test)
-                    results['fold'].append(fold)
                     results['score'].append(np.mean(score_test))
                     results['n_sample'].append(X_test[_idx_test].shape[0])
                     results['source'].append(target_domain)
@@ -112,11 +132,11 @@ for fold,(_,train) in enumerate(cv.split(features,targets,groups = groups)):
                     results['accuracy_train'].append(acc_trial_train)
                     results['accuracy_test'].append(acc_trial_test)
                     results['filename'].append(filename)
-                    [results[f'{property_name} T-{time_steps - ii} C-{jj}'].append(
-                        properties[jj,ii]
-                        ) for ii in range(time_steps) for jj in range(confidence_range)]
+                    [results[f'{property_name} T-{time_steps - ii}'].append(hidden_state_test.mean(0)[ii,0]) for ii in range(time_steps)]
             results_to_save = pd.DataFrame(results)
             results_to_save.to_csv(csv_saving_name,index = False)
+
+
 
 
 
--- scripts/4.2.2.simple_RNN_cross_domain_adequacy.py
+++ scripts/4.2.2.simple_RNN_cross_domain_adequacy.py
@@ -20,9 +20,9 @@ import pandas as pd
 
 from utils import build_RNN,scoring_func
 
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedShuffleSplit,LeaveOneGroupOut
 
-experiment          = ['confidence','cross_domain','RNN']
+experiment          = ['adequacy','cross_domain','RNN']
 property_name       = 'hidden states' # or hidden states or weight
 data_dir            = '../data/'
 model_dir           = os.path.join('../models',experiment[0],experiment[1],)
@@ -54,8 +54,8 @@ results             = dict(
                            source           = [],
                            sub_name         = [],
                            filename         = [],
-                           accuracy_train   = [],
-                           accuracy_test    = [],
+                           # accuracy_train   = [],
+                           # accuracy_test    = [],
                            )
 for ii in range(time_steps):
     results[f'{property_name} T-{time_steps - ii}'] = []
@@ -67,12 +67,12 @@ accuracies  = df_source['accuracy'].values
 
 csv_saving_name     = os.path.join(result_dir,f'{experiment[-1]}_{experiment[0]} results.csv')
 
-for acc_trial_train in [0,1]:
-    _idx, = np.where(accuracies == acc_trial_train)
-    X_,Y_,Z_ = features[_idx],targets[_idx],groups[_idx]
+csv_saving_name     = os.path.join(result_dir,f'{experiment[-1]}_{experiment[0]} results.csv')
+cv = LeaveOneGroupOut()
+for fold,(_,_train) in enumerate(cv.split(features,targets,groups = groups)):
+    X_,Y_,Z_ = features[_train],targets[_train],groups[_train]
     # the for-loop does not mean any thing, we only take the last step/output of the for-loop
-    for train,valid in StratifiedShuffleSplit(test_size = 0.2,
-                                                      random_state = 12345).split(X_,Y_,Z_):
+    for train,valid in StratifiedShuffleSplit(test_size = 0.2,random_state = 12345).split(X_,Y_,Z_):
         X_train,y_train,group_train,acc_train = X_[train],Y_[train],Z_[train],accuracies[train]
         X_valid,y_valid,gruop_valid,acc_valid = X_[valid],Y_[valid],Z_[valid],accuracies[valid]
     
@@ -112,25 +112,22 @@ for acc_trial_train in [0,1]:
         X_test           = to_categorical(X_test - 1, num_classes = confidence_range)
         y_test           = to_categorical(y_test - 1, num_classes = confidence_range)
         
-        for acc_trial_test in [0,1]:
-            _idx_test, = np.where(acc_test == acc_trial_test)
-            if len(_idx_test) > 1:
-                preds_test  = model.predict(X_test.astype('float32')[_idx_test], batch_size=batch_size)
-                print(f'get {property_name}')
-                hidden_state_test,h_state_test,c_state_test = hidden_model.predict(X_test[_idx_test],
-                                                                                    batch_size = batch_size,
-                                                                                    verbose = 1)
-                score_test = scoring_func(y_test[_idx_test],preds_test,
-                                          confidence_range = confidence_range)
-                print(score_test)
-                results['score'].append(np.mean(score_test))
-                results['n_sample'].append(X_test[_idx_test].shape[0])
-                results['source'].append(target_domain)
-                results['sub_name'].append(sub_name)
-                results['accuracy_train'].append(acc_trial_train)
-                results['accuracy_test'].append(acc_trial_test)
-                results['filename'].append(filename)
-                [results[f'{property_name} T-{time_steps - ii}'].append(hidden_state_test.mean(0)[ii,0]) for ii in range(time_steps)]
+        preds_test  = model.predict(X_test.astype('float32'), batch_size=batch_size)
+        print(f'get {property_name}')
+        hidden_state_test,h_state_test,c_state_test = hidden_model.predict(X_test,
+                                                                            batch_size = batch_size,
+                                                                            verbose = 1)
+        score_test = scoring_func(y_test,preds_test,
+                                  confidence_range = confidence_range)
+        print(score_test)
+        results['score'].append(np.mean(score_test))
+        results['n_sample'].append(X_test.shape[0])
+        results['source'].append(target_domain)
+        results['sub_name'].append(sub_name)
+        # results['accuracy_train'].append(acc_trial_train)
+        # results['accuracy_test'].append(acc_trial_test)
+        results['filename'].append(filename)
+        [results[f'{property_name} T-{time_steps - ii}'].append(hidden_state_test.mean(0)[ii,0]) for ii in range(time_steps)]
         results_to_save = pd.DataFrame(results)
         results_to_save.to_csv(csv_saving_name,index = False)
 
