1ab7ffe845037b282397bca512222e7de9a86f7b
nmningmei <semsociaty@gmail.com>
2021-04-24 08:21:02 +0200

    new analysis

---
+++ scripts/2.2.split data into past and recent.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 24 08:09:39 2021
+
+@author: nmei
+"""
+
+import os
+from glob import glob
+
+import pandas as pd
+import numpy as np
+
+working_dir = '../data/'
+working_data = glob(os.path.join(working_dir,'*','*','*.csv'))
+working_data = [item for item in working_data if ('confidence/' in item) or ('adequacy/' in item)]
+
+for f in working_data:
+    df = pd.read_csv(f)
+    df_past = df[['filename', 'sub', 'accuracy', 'feature1', 'feature2', 'feature3', 'targets']]
+    df_recent = df[['filename', 'sub', 'accuracy', 'feature5', 'feature6', 'feature7', 'targets']]
+    
+    df_past.to_csv(f.replace('.csv','_past.csv'),index = False)
+    df_recent.to_csv(f.replace('.csv','_recent.csv'),index = False)
